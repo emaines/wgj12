b8175ed308b40e155611bf4b3629e8be7b160e5d
E Maines <emaines@live.com>
2017-09-30 18:06:03 -0400

    Collision with truck deletes player

---
+++ Assets/Scripts/DestroyOnContact.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnContact : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        //if (other.tag == "Boundary")
+        //    return;
+
+        //Instantiate(explosion, transform.position, transform.rotation);
+        //if (other.tag == "Player")
+        //{
+        //    Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+        //    gameController.GameOver();
+        //}
+
+        //if (gameController)
+        //    gameController.AddScore(pointsValue);
+
+        //Destroy(other.gameObject);
+
+
+        if (other.tag == "Fatal")
+            Destroy(gameObject, 1);
+    }
+
+
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/GameDirector.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameDirector : MonoBehaviour {
+
+
+    private int score;
+    private bool gameOver = false;
+    private bool restart = false;
+
+    public GameObject[] hazards;
+    //public int harzardCount;
+
+    public float startWait;
+    public float spawnWait;
+    public Vector3 spawnValues;
+
+    //public GUIText scoreText;
+    //public GUIText gameOverText;
+    //public GUIText restartText;
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(SpawnHarzards());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator SpawnHarzards()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            Vector3 spawnPosition = new Vector3(Random.Range(- spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+
+            GameObject hazard = hazards[Random.Range(0, hazards.Length)];
+            if (hazard.name == "Pot Hole") spawnValues.z = -spawnValues.z;
+
+
+            Instantiate(hazards[Random.Range(0, hazards.Length)], spawnPosition, Quaternion.identity);
+
+            yield return new WaitForSeconds(spawnWait);
+        }
+
+    }
+
+}
