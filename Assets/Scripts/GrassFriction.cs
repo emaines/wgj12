0ec71f31d6eb2f5ec3ffd506ba872c619d7520b9
E Maines <emaines@live.com>
2017-10-04 19:44:18 -0400

    art for car, cones and trucks

---
+++ Assets/Scripts/GrassFriction.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassFriction : MonoBehaviour {
+
+    private bool enteredFrictionZone = false;
+
+    private GameObject playerPhysics;
+    private CustomPhysics customPhysics;
+
+    void Start()
+    {
+        playerPhysics = GameObject.Find("Player Physics");
+        if (playerPhysics)
+            customPhysics = playerPhysics.GetComponent<CustomPhysics>();
+    }
+
+	// Use this for initialization
+	void OnTriggerEnter () {
+        customPhysics.SetHighFriction(true);
+    }
+
+    // Use this for initialization
+    void OnTriggerExit()
+    {
+        customPhysics.SetHighFriction(false);
+
+    }
+
+}
